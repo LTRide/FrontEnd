04ff0880a64706672a956641f9d5dd95c05b36d0
haydenlinyard <linyard.hayden@gmail.com>
2025-11-11 15:43:54 -0600

    rendered map, created zoom and pan functions

--- LTRideFrontendProto/src/mainframe/MainPanel.java
+++ LTRideFrontendProto/src/mainframe/MainPanel.java
@@ -2,17 +2,82 @@ package mainframe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
 import java.util.*;
 
 public class MainPanel extends JPanel implements Runnable
 {
 	private ArrayList<AbstractShape> scaffolding;
 	private AbstractShape ui;
+	private static final long serialVersionUID = 1L;
+	private double zoom = 1.0;
+	private int offsetX = 0, offsetY = 0;
+	private int dragStartX = 0, dragStartY = 0;
 	
 	public MainPanel()
 	{
 		setVisible(true);
+		
+		addMouseWheelListener(new MouseAdapter()
+			{
+				@Override
+				public void mouseWheelMoved(MouseWheelEvent e)
+				{
+					double oldZoom = zoom;
+					if (e.getPreciseWheelRotation() < 0)
+						zoom *= 1.1;
+					else
+						zoom /= 1.1;
+					
+			        // Get mouse position
+			        int mouseX = e.getX();
+			        int mouseY = e.getY();
+
+			        // Adjust offset so zoom centers on mouse
+			        offsetX = (int) (mouseX - ((mouseX - offsetX) * (zoom / oldZoom)));
+			        offsetY = (int) (mouseY - ((mouseY - offsetY) * (zoom / oldZoom)));
+					
+					repaint();
+				}
+			});
+		
+        addMouseListener(new MouseAdapter() 
+        {
+            @Override
+            public void mousePressed(MouseEvent e) 
+            {
+                dragStartX = e.getX() - offsetX;
+                dragStartY = e.getY() - offsetY;
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() 
+        {
+            @Override
+            public void mouseDragged(MouseEvent e) 
+            {
+                offsetX = e.getX() - dragStartX;
+                offsetY = e.getY() - dragStartY;
+                repaint();
+            }
+        });
+		
 		scaffolding = new ArrayList<AbstractShape>();
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if (Stage.getMap() != null)
+		{
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.clearRect(0, 0, getWidth(), getHeight());
+			g2d.translate(offsetX, offsetY);
+			g2d.scale(zoom, zoom);
+			g.drawImage(Stage.getMap(), 0, 0, this);
+		}
 	}
 	
 	@Override
--- LTRideFrontendProto/src/mainframe/Stage.java
+++ LTRideFrontendProto/src/mainframe/Stage.java
@@ -2,14 +2,23 @@ package mainframe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class Stage extends JFrame
 {
 	private static final int WIDTH = 540;
 	private static final int HEIGHT = 960;
+	private static BufferedImage map = null;
 	
 	public static void main(String[] args)
 	{
+		try
+		{
+			setMap(ImageIO.read(new File("H:\\FrontEnd\\LTRideFrontendProto\\Assets\\Map Assets\\Zoomable School Map Proto.png")));
+		} catch (Exception e) {e.printStackTrace();}
+		
 		Stage run = new Stage();
 	}
 	
@@ -21,5 +30,19 @@ public class Stage extends JFrame
 		getContentPane().add(new MainPanel());
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Graphics2D g2d = getMap().createGraphics();
+		g2d.drawImage(getMap(), 0, 0, null);
+		g2d.dispose();
+	}
+
+	public static BufferedImage getMap() 
+	{
+		return map;
+	}
+
+	public static void setMap(BufferedImage map) 
+	{
+		Stage.map = map;
 	}
 }
